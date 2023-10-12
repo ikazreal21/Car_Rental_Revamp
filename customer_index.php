710d8d254419be1e1a6885af33c4ffa3652f828f
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2023-10-13 03:18:53 +0800

    first commit

---
+++ customer_index.php
@@ -0,0 +1,148 @@
+<?php
+session_start();
+require 'connection.php';
+$conn = Connect();
+
+$sql1 = "SELECT * FROM cars WHERE car_availability='yes'";
+$result1 = mysqli_query($conn, $sql1);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Axl Rentals</title>
+    <link rel="shortcut icon" type="image/png" href="assets/img/P.png">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/user.css">
+    <link rel="stylesheet" href="assets/w3css/w3.css">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation" style="color: black">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                    </button>
+                <a class="navbar-brand page-scroll" href="">
+                   Axl Rentals </a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="customer_index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="#"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $_SESSION['login_customer']; ?></a>
+                    </li>
+                    <ul class="nav navbar-nav">
+            <li><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> Garagge <span class="caret"></span> </a>
+                <ul class="dropdown-menu">
+              <li> <a href="prereturncar.php">Return My Car</a></li>
+              <li> <a href="mybookings.php"> My Bookings</a></li>
+            </ul>
+            </li>
+          </ul>
+                    <li>
+                        <a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <div id="sec2" style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
+
+        <h3 style="text-align:center;">Available Cars</h3>
+        <form action="" method="post">
+        <div class="row">
+            <div class="form-group col-md-3">
+                <select class="form-control">
+                    <option selected>Car Brand</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+            </div>
+            <div class="form-group col-md-3">
+                <button type="submit"name="submit" value="Rent Now" class="btn">Search</button>
+            </div>
+        </div>
+
+        </form>
+    <br>
+        <section class="menu-content">
+            <?php
+$sql1 = "SELECT * FROM cars WHERE car_availability='yes'";
+$result1 = mysqli_query($conn, $sql1);
+
+if (mysqli_num_rows($result1) > 0) {
+    while ($row1 = mysqli_fetch_assoc($result1)) {
+        $car_id = $row1["car_id"];
+        $car_name = $row1["car_name"];
+        $car_img = $row1["car_img"];
+
+        ?>
+            <a href="booking.php?id=<?php echo ($car_id) ?>">
+            <div class="sub-menu">
+
+
+            <img class="card-img-top" src="<?php echo $car_img; ?>" alt="Card image cap">
+            <h5><b> <?php echo $car_name; ?> </b></h5>
+            <h6> Rent Fee per Day: P1000</h6>
+
+
+            </div>
+            </a>
+            <?php }} else {
+    ?>
+<h1> No cars available :( </h1>
+                <?php
+}
+?>
+        </section>
+
+    </div>
+
+    <!-- <div class="bgimg-2">
+        <div class="caption">
+            <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;"></span>
+        </div>
+    </div> -->
+
+
+    <!-- Container (Contact Section) -->
+    <!-- -->
+    <footer class="site-footer">
+        <div class="container">
+            <hr>
+            <div class="row">
+                <div class="col-sm-6">
+                    <h5>© <?php echo date("Y"); ?> Axl Rentals</h5>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCCuoe93lQkgRaC7FB8fMOr_g1dmMRwKng&callback=myMap" type="text/javascript"></script>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <!-- Plugin JavaScript -->
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <!-- Custom Theme JavaScript -->
+    <script src="assets/js/theme.js"></script>
+</body>
+
+</html>
---
+++ login_customer.php
@@ -0,0 +1,62 @@
+<?php
+session_start(); // Starting Session
+$error=''; // Variable To Store Error Message
+
+if (isset($_POST['submit'])) {
+if (empty($_POST['customer_username']) || empty($_POST['customer_password'])) {
+$error = "Username or Password is invalid";
+}
+else
+{
+// Define $username and $password
+$customer_username=$_POST['customer_username'];
+$customer_password=$_POST['customer_password'];
+// Establishing Connection with Server by passing server_name, user_id and password as a parameter
+require 'connection.php';
+$conn = Connect();
+
+// SQL query to fetch information of registerd users and finds user match.
+$query = "SELECT * FROM customers WHERE customer_username='$customer_username' AND customer_password='$customer_password' LIMIT 1";
+
+// To protect MySQL injection for Security purpose
+// $stmt2 = $conn->prepare($query);
+// $stmt2 -> bind_param("ss", $customer_username, $customer_password);
+// $stmt2 -> execute();
+// $stmt2 -> bind_result($customer_username, $customer_password);
+// $stmt2 -> store_result();
+// $stmt2->fetch();
+$stmt2 = mysqli_query($conn, $query);
+
+$row = mysqli_fetch_assoc($stmt2);
+
+
+// echo '<pre>';
+// var_dump($row);
+// echo '<pre>';
+
+if (mysqli_num_rows($stmt2) == 1) 
+{
+	
+	if ($row['role'] == "admin") {
+		$_SESSION['login_client']=$customer_username;
+	
+		header("Location:index.php");
+
+		exit();
+	}	
+	else if($row['role'] == "customer") {
+		$_SESSION['login_customer']=$customer_username;
+
+		
+		header("Location:customer_index.php");
+
+		exit();
+
+	}
+} else {
+$error = "Username or Password is invalid";
+}
+mysqli_close($conn); // Closing Connection
+}
+}
+?>
