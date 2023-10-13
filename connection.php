601f37d14126ce28bcc39c36c56b70b58eb26428
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2023-10-14 02:28:32 +0800

    database

--- connection.php
+++ connection.php
@@ -2,10 +2,10 @@
 
 function Connect()
 {
-	$dbhost = "localhost";
-	$dbuser = "root";
-	$dbpass = "";
-	$dbname = "db_carrental";
+	$dbhost = "us-cdbr-east-06.cleardb.net";
+	$dbuser = "be2d290de4e5b0";
+	$dbpass = "5a51f367";
+	$dbname = "heroku_2e0d3617f5a68cb";
 
 	//Create Connection
 	$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname) or die($conn->connect_error);
